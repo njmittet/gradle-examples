835af0d3c8069b600a04ceb4762625181ac8ce9c
Nils Jørgen Mittet <njmittet@gmail.com>
2019-02-15 10:37:53 +0100

    Add MathLibrary tests

--- m2-repo-test/math-library/src/main/java/no/njm/math/MathLibrary.java
+++ m2-repo-test/math-library/src/main/java/no/njm/math/MathLibrary.java
@@ -1,7 +1,17 @@
 package no.njm.math;
 
+/**
+ * Class providing a set of common math operations.
+ */
 public class MathLibrary {
 
+    /**
+     * Sums two signed integer values.
+     *
+     * @param x The first value.
+     * @param y The second value.
+     * @return The sum of the provided values.
+     */
     public int add(int x, int y) {
         return x + y;
     }
--- m2-repo-test/math-library/src/test/java/no/njm/math/MathLibraryTest.java
+++ m2-repo-test/math-library/src/test/java/no/njm/math/MathLibraryTest.java
@@ -1,14 +1,38 @@
 package no.njm.math;
 
-import no.njm.math.MathLibrary;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class MathLibraryTest {
-    
-    @Test 
-    public void addTwoNumbersSuccessfully() {
+
+    @Test
+    public void addTwoNPositivityNumbers() {
         MathLibrary math = new MathLibrary();
-        assertEquals("2 + 2 is 4", 4, math.add(2, 2));
+        assertEquals("2 + 2 should be 4", 4, math.add(2, 2));
+    }
+
+    @Test
+    public void addTwoNegativeNumbers() {
+        MathLibrary math = new MathLibrary();
+        assertEquals("-2 + -3 should be -5", -5, math.add(-2, -3));
+    }
+
+    @Test
+    public void addOnePositiveAndOneNegativeNumber() {
+        MathLibrary math = new MathLibrary();
+        assertEquals("-2 + 3 should be 1", 1, math.add(-2, 3));
+    }
+
+    @Test
+    public void addOnePositiveNumberToZero() {
+        MathLibrary math = new MathLibrary();
+        assertEquals("2 + 0 should be 2", 2, math.add(2, 0));
+    }
+
+    @Test
+    public void addTwoZeros() {
+        MathLibrary math = new MathLibrary();
+        assertEquals("0 + 0 should be 0", 0, math.add(0, 0));
     }
 }
